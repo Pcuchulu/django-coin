23784b662522c534e33598dcf2b6fa9305b0fdf8
sami ka <you@example.com>
2024-05-14 04:26:35 +0330

    tests

--- aban/abancoin/tests.py
+++ aban/abancoin/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Coin
 
-# Create your tests here.
+class CoinModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+        self.coin = Coin.objects.create(user=self.user, name='aban', quantity=5, price=50000)
+
+    def test_coin_creation(self):
+        coin = Coin.objects.get(name='aban')
+        self.assertEqual(coin.quantity, 5)
+        self.assertEqual(coin.price, 50000)
+        self.assertEqual(coin.user, self.user)
+
+    def test_coin_str(self):
+        coin = Coin.objects.get(name='aban')
+        self.assertEqual(str(coin), 'aban - 5')
